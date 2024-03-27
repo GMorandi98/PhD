5390a5748f723de26ebb40af42f2dd6e0fc61bf9
GMorandi98 <g.morandi1@campus.unimib.it>
2024-03-27 15:40:31 +0100

    Added plot for 't-t0=const' cases

--- Data_Analysis/Pure_gauge/GEVP_pyobs.py
+++ Data_Analysis/Pure_gauge/GEVP_pyobs.py
@@ -4,8 +4,9 @@ import pyobs
 def GEVP(C, t0=-1, const=1, case='ratio'): 
     Noss, Noss1, D = C.shape
     assert (Noss == Noss1), f"Check shape of 'C': non-square matrix along axes=(0,1)."
-    
-    if t0 == -1:        ### CASE 1  :  t-t0=const, fix const, span t,t0 values. ###
+
+    ### CASE 1  :  t-t0=const, fix const, span t,t0 values. ###
+    if t0 == -1:        
         rx = np.arange(D-const)
         lx  = rx + const
         ground  = []
@@ -22,13 +23,15 @@ def GEVP(C, t0=-1, const=1, case='ratio'):
                 [lam, rv, lv] = pyobs.linalg.eigLR(Ct0_inv @ Ct)
                 E0 = (-pyobs.log(lam)/const)[-1]    
                 e, de = E0.error()
-                if np.abs(de/e) >= 1.0:
+                if np.abs(de/e) >= 1.00:
                     break
                 ground.append(E0)
                 times.append(np.array([t, _t0]))
             except:
                 print(f"(t,t0)=({t},{_t0})  :  Array containing Infs or NaNs.\n")
-    else:               ### CASE 2  :  fix t0 and span t values. ###
+
+    ### CASE 2  :  fix t0 and span t values. ###
+    else:               
         assert (isinstance(t0, int) and t0 >= 0 and t0 < D-1), \
             f"'t0' should be an integer in between 0 and {D-1}, got t0={t0}."
         Ct0_inv = pyobs.linalg.inv(pyobs.remove_tensor(C[:,:,t0]))
@@ -49,7 +52,7 @@ def GEVP(C, t0=-1, const=1, case='ratio'):
                     [lam1, rv1, lv1] = pyobs.linalg.eigLR(Ct0_inv @ Ct1)
                     E0 = pyobs.log(lam/lam1)[-1]
                     e, de = E0.error()
-                    if np.abs(de/e) >= 1.0:
+                    if np.abs(de/e) >= 1.00:
                         break
                     ground.append(E0)
                     times.append(t)
@@ -65,7 +68,7 @@ def GEVP(C, t0=-1, const=1, case='ratio'):
                     [lam,  rv,  lv]  = pyobs.linalg.eigLR(Ct0_inv @ Ct)
                     E0 = (-pyobs.log(lam)/(t-t0))[-1]
                     e, de = E0.error()
-                    if np.abs(de/e) >= 1.0:
+                    if np.abs(de/e) >= 1.00:
                         break
                     ground.append(E0)
                     times.append(t)
